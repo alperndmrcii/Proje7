bff59cc0a2bafc868e36dc7280dea7000aaa0e5d
Alperen Demirci <alperndmrcii@gmail.com>
2023-02-24 19:31:48 +0300

    Selenium Test Project Started

---
+++ src/Proje7/proje.java
@@ -0,0 +1,16 @@
+package Proje7;
+
+import Utility.BaseDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Driver;
+
+public class proje extends BaseDriver {
+    public static void main(String[] args) {
+        driver.get("https://itera-qa.azurewebsites.net/");
+
+    }
+}
